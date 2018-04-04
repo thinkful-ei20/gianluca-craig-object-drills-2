6f9192f116f18b80f16844f0a1aac3e9e54aca16
Craig Pounds <nweaver@windstream.net>
2018-04-04 12:04:24 -0500

    testing validateKeys()

--- gianluca-craig-object-drills-2.js
+++ gianluca-craig-object-drills-2.js
@@ -47,3 +47,35 @@ const scratchData = [
 function findById(items, idNum) {
   return items.find(e => idNum === e.id);
 }
+
+// running the function with `objectA` and `expectedKeys`
+// should return `true`
+const objectA = {
+  id: 2,
+  name: 'Jane Doe',
+  age: 34,
+  city: 'Chicago',
+};
+
+// running the function with `objectB` and `expectedKeys`
+// should return `false`
+const objectB = {
+  id: 3,
+  age: 33,
+  city: 'Peoria',
+};
+
+const expectedKeys = ['id', 'name', 'age', 'city'];
+
+function validateKeys(object, expectedKeys) {
+  let validation = true;
+  expectedKeys.forEach(e => {
+    for (let o in Object.keys(object)) {
+      if (e !== o) {
+        validation = false;
+        break;
+      }
+    }
+  });
+  return validation;
+}
